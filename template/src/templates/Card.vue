f3999df8070a6029305b3c6be32a5683be86b481
littlefatcat <littlefatcat@163.com>
2024-02-06 10:40:18 +0800

    feat(template): sync 1.2.6 theme version

---
+++ template/src/templates/Card.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card text-bg-primary" style="width: 18rem">
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">
+                {{ subtitle }}
+            </h6>
+            <p class="card-text">{{ text }}</p>
+            <a :href="uriFor([link])" class="card-link">点击查看更多</a>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { uriFor } from 'hexo-theme-lnote/templates/utils/uri'
+const title = ''
+const subtitle = ''
+const text = ''
+const link = ''
+</script>
